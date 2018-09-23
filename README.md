d9e2f0e53c2d7f75fd631c3df4098cb3446c2bd6
ma-za-kpe <makpalyy@gmail.com>
2018-09-23 20:55:21 +0300

    Add tests for GET /todos

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # advanced-todo-app
+
+## writing tests using
+1. expect for assertions
+2. mocha, the entire test suite
+3. super test, to test the express routes
+4. nodemon, lets us create the test watch script.
+
+### all the above modules are installed and saved using 
+npm i <above modules> --save-dev 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,12 +4,17 @@ const	request	=	require('supertest');
 const	{app}	=	require('./../server');
 const	{Todo}	=	require('./../models/todo');
 
+const todos = [
+    {text:'code till 3pm'}, 
+    {text:'swim till 7pm'}
+];
+
 beforeEach((done) => {
     //this is a statement syntax
     //Todo.remove({}).then(() => done());
     Todo.remove({}).then(()	=> {
-        done();
-    })
+      return Todo.insertMany(todos);
+    }).then(() => done());
 
 });
 
@@ -28,7 +33,7 @@ describe('POST	/todos', ()	=> {
             if(err)	{
 			    return	done(err);
             }
-            Todo.find().then((todos)	=>	{
+            Todo.find({text}).then((todos)	=>	{
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -47,11 +52,24 @@ describe('POST	/todos', ()	=> {
 			 return done(err);
             }
             Todo.find().then((todos) =>	{
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
              }).catch((e) => done(e));
 		});
     });
 
 
+});
+
+
+describe('GET /todos', () => {
+    it('should	get	all	todos',	(done) => {
+            request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(todos.length).toBe(2);
+            })
+            .end(done);
+    });
 });
